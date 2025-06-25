f831f67fae3442f34e964715670d0fc9909973dc
DESKTOP-D8N6CJF\Sadra <sadra_h_m@outlook.com>
2025-06-25 23:35:27 +0200

    Projectile hit events added.

--- Source/BlastZone/Weapon/Projectile.h
+++ Source/BlastZone/Weapon/Projectile.h
@@ -14,9 +14,13 @@ class BLASTZONE_API AProjectile : public AActor
 public:	
 	AProjectile();
 	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
 
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private:
 	UPROPERTY(EditAnywhere)
@@ -28,8 +32,13 @@ private:
 	UPROPERTY(EditAnywhere)
 	class UParticleSystem* TrailEffect;
 
-	UPROPERTY(EditAnywhere)
 	class UParticleSystemComponent* TrailComponent;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
 
 public:	
 
